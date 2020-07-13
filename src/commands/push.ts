5f3f64fd1f30754ad47a28263b283ec4e9f50360
Michel Nemnom <1678801+Pegase745@users.noreply.github.com>
2020-07-13 12:12:30 +0200

    feat(push): add new options to the push command
    - `--merge`: to merge strings with the ones on the server (default:
      true)
    - `--no-merge`
    - `--label`: label to assign all changes made during this update
    - `--ignore-missing`: disable obsoleting missing strings (default: true)
    - `--no-ignore-missing`

--- src/commands/push.ts
+++ src/commands/push.ts
@@ -1,13 +1,32 @@
 import cli from 'cli-ux';
-import { Command } from '@oclif/command';
+import { Command, flags } from '@oclif/command';
 
 import { Project, MasterFile } from '../models';
 
 export default class Push extends Command {
   static description = 'push master language file';
 
+  static flags = {
+    merge: flags.boolean({
+      description: 'merge strings with the ones on the server (default: true)',
+      default: true,
+      // may be reversed with --no-merge to override server values
+      allowNo: true,
+    }),
+    label: flags.string({
+      description: 'label to assign all changes made during this update',
+    }),
+    'ignore-missing': flags.boolean({
+      description: 'disable obsoleting missing strings (default: true)',
+      default: true,
+      // may be reversed with --no-ignore-missing to obsolete missing strings
+      allowNo: true,
+    }),
+  };
+
   async run() {
     const { projectFiles } = await Project.init();
+    const { flags } = this.parse(Push);
 
     let masterFile = projectFiles.filter(
       (file) => file.master_project_file_id === null
@@ -17,7 +36,8 @@ export default class Push extends Command {
 
     await new MasterFile(masterFile.id).update(
       masterFile.name,
-      masterFile.locale_code
+      masterFile.locale_code,
+      flags
     );
 
     cli.action.stop(
