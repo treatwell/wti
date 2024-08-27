f62ece6f4c6f7067e7f6e799c82463885bf113be
Simon Smith <simon.smith@treatwell.com>
2024-08-27 17:47:13 +0100

    fix: allow update of more than one master file

--- src/commands/pull.ts
+++ src/commands/pull.ts
@@ -11,28 +11,37 @@ export default class Pull extends Command {
     configPath: flags.string({
       name: 'configPath',
       required: false,
-      description: 'Path to wti-config.json file. If not provided, default to git root directory.'
-    })
+      description:
+        'Path to wti-config.json file. If not provided, default to git root directory.',
+    }),
   };
 
   async run() {
-    const { flags: { configPath } } = this.parse(Pull);
+    const {
+      flags: { configPath },
+    } = this.parse(Pull);
     const { projectFiles } = await Project.init(configPath);
 
-    let masterFile = projectFiles.filter(
-        (file) => file.master_project_file_id === null
-      )[0],
-      masterFileId = masterFile.id;
-
+    const masterFiles = projectFiles.filter(
+      (file) => file.master_project_file_id === null
+    );
     const tasks = new Listr(
-      projectFiles.reduce(
-        (acc, current) =>
-          acc.concat({
-            title: `Pulling ${current.name}`,
-            task: async () => await new MasterFile(masterFileId).show(current, configPath),
-          }),
-        [] as Listr.ListrTask<Listr.ListrContext>[]
-      )
+      projectFiles.reduce((acc, current) => {
+        const masterFileToUpdate = masterFiles.find(
+          (file) => file.id === current.master_project_file_id
+        );
+        if (!masterFileToUpdate) {
+          return acc;
+        }
+        return acc.concat({
+          title: `Pulling ${current.name}`,
+          task: async () =>
+            await new MasterFile(masterFileToUpdate?.id).show(
+              current,
+              configPath
+            ),
+        });
+      }, [] as Listr.ListrTask<Listr.ListrContext>[])
     );
 
     tasks.run().catch((err) => {
--- src/commands/push.ts
+++ src/commands/push.ts
@@ -25,29 +25,31 @@ export default class Push extends Command {
     configPath: flags.string({
       name: 'configPath',
       required: false,
-      description: 'Path to wti-config.json file. If not provided, default to git root directory.'
-    })
+      description:
+        'Path to wti-config.json file. If not provided, default to git root directory.',
+    }),
   };
 
   async run() {
-    const { flags : { configPath, ...flags } } = this.parse(Push);
+    const {
+      flags: { configPath, ...flags },
+    } = this.parse(Push);
     const { projectFiles } = await Project.init(configPath);
 
-    let masterFile = projectFiles.filter(
+    const masterFiles = projectFiles.filter(
       (file) => file.master_project_file_id === null
-    )[0];
-
-    cli.action.start(`Pushing master file ${masterFile.name}...`);
-
-    await new MasterFile(masterFile.id).update(
-      masterFile.name,
-      masterFile.locale_code,
-      flags,
-      configPath
     );
 
-    cli.action.stop(
-      'done. Note that it can take up to 1 min for 1000 segments to be uploaded'
-    );
+    console.log(`${masterFiles.length} master file(s) to process...`);
+    for (const masterFile of masterFiles) {
+      console.log(`Pushing ${masterFile.name}...`);
+      await new MasterFile(masterFile.id).update(
+        masterFile.name,
+        masterFile.locale_code,
+        flags,
+        configPath
+      );
+    }
+    console.log('done.');
   }
 }
